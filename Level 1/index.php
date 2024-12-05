36320a0dc0d96c06f546583bf0acd2c7a783b3be
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-12-05 09:26:00 +0100

    feat: setup tailwind framework

--- Level 1/index.php
+++ Level 1/index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>level 1</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </body>
 </html>
